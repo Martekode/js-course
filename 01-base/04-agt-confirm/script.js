5fd8d9527ae895e7cff1f70a8aae0c0d6a9f663c
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-06 14:09:21 +0200

    base done

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -9,8 +9,21 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function main(name,age,location) {
+ 
+    prompt('what is your name');
+    prompt('what is your age');
+    prompt('what is your location');
 
+    
+    
+    
+    let confirmation = prompt('is this data correct? (confirm or not-confirmed)', name + ', ' + age + ', '+ location);
+    if (confirmation === 'confirm'){
+        alert('confirmed');
+    }
+    else{
+        main();
+    };
     // your code here
-
 })();
