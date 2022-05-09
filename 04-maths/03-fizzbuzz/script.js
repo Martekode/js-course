1a8953ead3bb16add06b744700d9e31da5c7d297
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-09 11:03:35 +0200

    made it even better

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -14,13 +14,13 @@
     // your code here
     for (i=1;i<101;i++){
         if (i % 3 ===0){
-            console.log("fizz");
+            console.log(i + " is fizz");
         }
         if (i % 5 ===0){
-            console.log("buzz");
+            console.log(i+" is buzz");
         }
         if (i % 5 ===0 && i % 3 ===0 ){
-            console.log("fizzbuzz");
+            console.log(i+" is fizzbuzz");
         }
         if (i %5 !== 0 && i % 3 !== 0 ){
             console.log(i);
