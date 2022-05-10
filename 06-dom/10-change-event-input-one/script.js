4e49b3c844a1ff6bf900f2cc599a65d07c616608
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-10 11:23:42 +0200

    end of dom

--- 06-dom/10-change-event-input-one/script.js
+++ 06-dom/10-change-event-input-one/script.js
@@ -10,7 +10,18 @@
 // You will have time to focus on it later.
 
 (function() {
-
+ const myInputfield = document.getElementById('pass-one');
+ var text = document.getElementById('pass-one').value;
+console.log(text);
+myInputfield.maxLength = 10;
+ myInputfield.addEventListener('input',()=>{
+    text = document.getElementById('pass-one').value;
+    console.log(text.length);
+    
+     if (text.length> myInputfield.maxLength){
+         text.length - 1;
+     }
+ })
     // your code here
 
 })();
