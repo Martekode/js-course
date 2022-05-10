34b84aa97fb1d93747cbad11d55f642c81244d28
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-10 10:10:55 +0200

    took a long time byt found out onmousehoverand onmouseout

--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -6,11 +6,26 @@
  * started at 26/10/2018
  */
 
+
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
 (function() {
-
+    
+    var image = document.getElementsByTagName("img")[0];
+    const b = image.getAttribute("src");
+    var a = image.getAttribute("data-hover");
+    //console.log(image);
     // your code here
-
+    image.onmouseover = function(){
+        //console.log('lukt');
+        //console.log(a);
+        image.setAttribute('src', a);
+        console.log(image);
+    }
+    image.onmouseout = function (){
+        //console.log(b);
+        image.setAttribute("src", b);
+        //console.log(image);
+    }
 })();
