095d28556fec6dd532f53c6c3d914f5c26cf597d
Marteko9000 <themarteko@protonmail.com>
2022-05-11 12:26:41 +0200

    figuring it out 7.2

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -10,5 +10,14 @@
 // You will have time to focus on it later.
 
 (() => {
+    document.getElementById('run').addEventListener('click', () => {
+         let a = document.getElementById('hero-id').value;7
+         let b = document.getElementById('target');
+         let c = document.getElementById('tpl-hero').content;
+         var d = document.importNode(c,true);
+         fetch('http://localhost:3000/heroes/'+a).then(res => res.json()).then(data => b.appendChild(c)).catch();
+         console.log(c);
+        
+    })
     // your code here
 })();
