b964ecfd2ab74b19148b0d4f0dddb6f9aef3b956
Marteko9000 <themarteko@protonmail.com>
2022-05-11 16:47:50 +0200

    7.2 finished, added .catch

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -24,7 +24,7 @@
             power.innerText = data.abilities;
             
             b.appendChild(clone);
-        });
+        }).catch(err => alert('that id does not exist'));
         
          
          
