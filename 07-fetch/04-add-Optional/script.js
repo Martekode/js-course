64acf37846aad7203c90f11faeeb3e11f229dfd8
Marteko9000 <themarteko@protonmail.com>
2022-05-12 14:23:44 +0200

    7.4 finished, did real post as if real api

--- 07-fetch/04-add-Optional/script.js
+++ 07-fetch/04-add-Optional/script.js
@@ -10,5 +10,27 @@
 // You will have time to focus on it later.
 
 (() => {
+    document.querySelector('form').addEventListener("submit",function(e){
+        e.preventDefault();
+        var a = document.getElementById('hero-name').value;
+        var b = document.getElementById('hero-alter-ego').value;
+        var c = document.getElementById('hero-powers').value;
+        var data = {name: a,alterEgo: b,abilities:[c]};
+
+
+        postData(data);
+
+        
+
+    })
+    async function postData(data){
+        console.log(JSON.stringify(data));
+        await fetch('http://localhost:3000/heroes/', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            
+        })
+        console.log(JSON.stringify(data));
+    }   
     // your code here
 })();
