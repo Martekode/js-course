c2fd41ac79b175c73a4b768884bdefe8f38b77a2
Martekode <BrianDesmedt9000@hotmail.com>
2022-05-13 12:18:13 +0200

    waveText ex: everyletter increases in fontSize

--- optional/01-misc/01-waving-text/script.js
+++ optional/01-misc/01-waving-text/script.js
@@ -10,20 +10,42 @@
 // You will have time to focus on it later.
 
 (function() {
- const a = document.getElementById('target').innerHTML;
-    //console.log(a)
-    function mkarray(string){
-        return string.split('');
-    }
-    let b = mkarray(a);
-    //console.log(b);
-    function mkwave(wave){
-        for(i=0,z=10;i<wave.length;i++,z++){
-            //console.log(wave[i]);
-          return wave[i].style.fontSize = `${z}px`;
-        }
-    }
+ /*const a = document.getElementById('target');
+ let target = document.getElementById('target');
+    console.log(a)
+    let b = a.innerHTML.split("").map(letter => {
+        console.log(letter);
+        let span = document.createElement('span');
+        span.innerText = letter;
+    })
+    .join("");
+    console.log(b);
+    //het werkt tot hier, alles hierna doet niets
     
-    mkwave(b);
+    
+    function mkwaves(wave){
+     for(i=0,z=10;i<wave.length;i++,z+=2){
+        console.log(wave[i]);
+        return wave[i].style.fontSize = `${i+z}px`;
+        
+     }
+    }
+    let d = document.getElementsByTagName('span');
+    console.log(d);
+    mkwaves(document.getElementsByTagName('span'));*/
 
+
+    const text = document.getElementById('target');
+    const strTxt = text.textContent;
+    const spltTxt = strTxt.split("");
+    text.textContent = "";
+    console.log(spltTxt);
+
+    for (i=0,z=10;i<spltTxt.length;i++,z++){
+        text.innerHTML += "<span>"+ spltTxt[i]+"</span>";
+        let span = text.querySelectorAll('span')[i];
+        span.style.fontSize = `${i+z}px`
+    }
+
+    
 })();
